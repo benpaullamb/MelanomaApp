eb04ccc85a8568ae443d8a19d8b95060849b70ac
Ben <benpaullamb@gmail.com>
2020-02-06 21:13:48 +0000

    Made much more advanced image reading into 1 4D tensor

--- TensorFlow/imageIO.js
+++ TensorFlow/imageIO.js
@@ -3,31 +3,150 @@ const tfn = require('@tensorflow/tfjs-node');
 const fs = require('fs');
 const path = require('path');
 
-async function loadImages(dir) {
-    console.log('Reading filenames');
-    const imageNames = await getImageNames(dir);
-    console.log(`${imageNames.length} images found`);
+async function loadImages(dir, maxImages) {
+    console.time('Load Images Duration');
 
-    console.log('Reading image data into tensors');
-    const imageTensors = await getImageTensors(dir, imageNames);
-    console.log(`${imageTensors.length} tensors created`);
+    const imageTensors = await getImageTensorsFromDir(dir, maxImages);
 
-    return imageTensors;
+    console.log('Resizing images');
+    const resizedImages = resizeImages(imageTensors, 500);
+    // console.log(`New shape: ${resizedImages[0].shape}`);
+
+    console.log('Batching tensors');
+    const tensor = batchTensors(resizedImages);
+    // console.log(`Batched tensor: ${tensor.shape}`);
+
+    console.log('Managing memory');
+    resizedImages.forEach(t => t.dispose());
+    imageTensors.forEach(t => t.dispose());
+    // console.log(`Number of tensors in memory: ${tf.memory().numTensors}`);
+
+    console.timeEnd('Load Images Duration');
+
+    return tensor;
 }
 
-async function getImageNames(path) {
-    return new Promise((res, rej) => {
-        fs.readdir(path, (err, imageNames) => {
+function saveTensorAsImages(tensor, dir) {
+    tf.tidy(() => {
+        const imageTensors = tf.split(tensor, tensor.shape[0]);
+        const imageTensors3d = imageTensors.map(image => tf.unstack(image)[0]);
 
-            if (err) return rej(err);
-
-            return res(imageNames);
+        imageTensors3d.forEach(async (image, i) => {
+            const jpeg = await tfn.node.encodeJpeg(image, 'rgb');
+            fs.writeFile(path.join(dir, `${i + 1}.jpg`), jpeg, err => {
+                if (err) return console.log(err);
+            });
         });
     });
 }
 
+function batchTensors(tensors) {
+    return tf.concat(tensors);
+}
+
+function resizeImages(tensors, size) {
+    cropSize = [size, size];
+    boxInd = [0];
+
+    tensors4d = tensors.map(tensor => tf.expandDims(tensor));
+
+    const resizedImages = tensors4d.map(image => {
+        const width = image.shape[2];
+        const height = image.shape[1];
+
+        let croppedSize = 0;
+        if (width < height) {
+            croppedSize = Math.floor(width / 100) * 100;
+        } else {
+            croppedSize = Math.floor(height / 100) * 100;
+        }
+
+        const y1 = (height / 2) - (croppedSize / 2);
+        const x1 = (width / 2) - (croppedSize / 2);
+
+        const y2 = (height / 2) + (croppedSize / 2);
+        const x2 = (width / 2) + (croppedSize / 2);
+
+        const boxes = [
+            [y1 / height, x1 / width, y2 / height, x2 / width]
+        ];
+
+        return tf.image.cropAndResize(image, boxes, boxInd, cropSize);
+    });
+
+    tensors4d.forEach(t => t.dispose());
+
+    return resizedImages;
+}
+
+async function printImagesSummary(dir, maxImages) {
+    console.time('Print Images Summary Duration');
+
+    const tensors = await getImageTensorsFromDir(dir, maxImages);
+
+    console.log('--------------------------------');
+    console.log(`Images Summary`);
+
+    const min = {
+        width: 10000,
+        height: 10000
+    };
+    const max = {
+        width: 0,
+        height: 0
+    };
+
+    const sizes = [200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100];
+    const countsAbove = {};
+    sizes.forEach(size => {
+        countsAbove[size] = 0;
+    });
+
+    tensors.forEach((tensor, i) => {
+        const width = tensor.shape[1];
+        const height = tensor.shape[0];
+
+        if (width < min.width) min.width = width;
+        if (height < min.height) min.height = height;
+
+        if (width > max.width) max.width = width;
+        if (height > max.height) max.height = height;
+
+        sizes.forEach(size => {
+            if (width >= size && height >= size) countsAbove[size]++;
+        });
+    });
+
+    tensors.forEach(t => t.dispose());
+
+    console.log(`Min: ${min.width}x${min.height}`);
+    console.log(`Max: ${max.width}x${max.height}`);
+    console.log(`Mean: ${min.width + ((max.width - min.width) / 2)}x${min.height + ((max.height - min.height) / 2)}`);
+
+    console.log(`% of images above each size:`);
+    sizes.forEach((size, i) => {
+        console.log(`${size}: ${((countsAbove[size] / tensors.length) * 100).toFixed(1)}%`);
+    });
+
+    console.timeEnd('Print Images Summary Duration');
+    console.log('--------------------------------');
+}
+
+async function getImageTensorsFromDir(dir, maxImages) {
+    console.log('Reading filenames');
+    const imageNames = await getImageNames(dir, maxImages);
+    console.log(`${imageNames.length} images found`);
+
+    console.log('Reading image data into tensors');
+    const tensors = await getImageTensors(dir, imageNames);
+    console.log(`${tensors.length} tensors created`);
+
+    return tensors;
+}
+
 async function getImageTensors(dir, imageNames) {
-    return Promise.all(imageNames.map(imageName => getImageTensor(path.join(dir, imageName))));
+    const res = await Promise.allSettled(imageNames.map(imageName => getImageTensor(path.join(dir, imageName))));
+    return res.map(obj => obj.value).filter(value => value);
 }
 
 async function getImageTensor(path) {
@@ -36,10 +155,33 @@ async function getImageTensor(path) {
 
             if (err) return rej(err);
 
-            const imageTensor = tfn.node.decodeJpeg(imageBuffer, 3);
-            return res(imageTensor);
+            try {
+                const imageTensor = tfn.node.decodeJpeg(imageBuffer, 3);
+                console.log(`Decoded: ${path}`);
+                return res(imageTensor);
+            } catch (err) {
+                return rej(err);
+            }
         });
     });
 }
 
-module.exports = loadImages;
+async function getImageNames(path, max) {
+    const res = await new Promise((res, rej) => {
+        fs.readdir(path, (err, imageNames) => {
+
+            if (err) return rej(err);
+
+            return res(imageNames);
+        });
+    });
+
+    if (max) res.length = max;
+    return res;
+}
+
+module.exports = {
+    loadImages,
+    printImagesSummary,
+    saveTensorAsImages
+};
--- TensorFlow/main.js
+++ TensorFlow/main.js
@@ -2,59 +2,13 @@ const tf = require('@tensorflow/tfjs');
 const tfn = require('@tensorflow/tfjs-node');
 const fs = require('fs');
 const path = require('path');
-const loadImages = require('./imageIO');
-
-// LeNet-5 architecture
-const model = tf.sequential();
-
-model.add(tf.layers.conv2d({
-    filters: 6,
-    kernelSize: 5,
-    stride: 1,
-    inputShape: [962, 888, 3],
-    batchSize: 1
-}));
-
-model.add(tf.layers.averagePooling2d({
-    poolSize: 2,
-    strides: 2
-}));
-
-model.add(tf.layers.conv2d({
-    filters: 16,
-    kernelSize: 5,
-    stride: 1
-}));
-
-model.add(tf.layers.averagePooling2d({
-    poolSize: 2,
-    strides: 2
-}));
-
-model.add(tf.layers.flatten());
-
-model.add(tf.layers.dense({
-    units: 84
-}));
-
-model.add(tf.layers.dense({
-    units: 2,
-    activation: 'softmax'
-}));
-
-const optimizer = tf.train.sgd(0.01);
-
-model.compile({
-    optimizer,
-    loss: 'meanSquaredError'
-});
+const { loadImages, printImagesSummary } = require('./imageIO');
 
 async function start() {
-    const tensors = await loadImages(path.join(__dirname, './MED-NODE-Dataset/Melanoma'));
-    // console.log(tensors[0]);
+    const imageDir = path.join(__dirname, './CatsAndDogs/Cat');
+    printImagesSummary(imageDir, 10);
 
-    // const ys = tf.tensor([0]);
-
-    // model.fit(tensors[0], ys);
+    // const tensor = await loadImages(imageDir, 10);
+    // console.log(tensor.shape);
 }
 start();
